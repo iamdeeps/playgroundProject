f91be5840bf3fb52e06088d69e9b93eabb7c9a44
Deepak <deepakbehki@gmail.com>
2021-04-06 01:02:12 +0530

    Initial Commit, Structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DragdropComponent } from './components/dragdrop/dragdrop.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DragdropComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
